a29a959baf733edd13394b15a0f40133595e7904
David Fowler <davidfowl@gmail.com>
2016-02-06 17:35:29 -0800

    Make ApplicationBuilder functional

--- src/ServerStack/ApplicationBuilder.cs
+++ src/ServerStack/ApplicationBuilder.cs
@@ -7,25 +7,82 @@ namespace ServerStack
 {
     public class ApplicationBuilder<TContext> : IApplicationBuilder<TContext>
     {
-        public IServiceProvider ApplicationServices { get; set; }
+        internal static string ServerFeaturesKey = "server.Features";
+        internal static string ApplicationServicesKey = "application.Services";
+
+        private readonly IList<Func<Func<TContext, Task>, Func<TContext, Task>>> _components = new List<Func<Func<TContext, Task>, Func<TContext, Task>>>();
+
+        public ApplicationBuilder(IServiceProvider serviceProvider)
+        {
+            Properties = new Dictionary<string, object>();
+            ApplicationServices = serviceProvider;
+        }
+
+        public ApplicationBuilder(IServiceProvider serviceProvider, object server)
+            : this(serviceProvider)
+        {
+            SetProperty(ServerFeaturesKey, server);
+        }
+
+        private ApplicationBuilder(ApplicationBuilder<TContext> builder)
+        {
+            Properties = builder.Properties;
+        }
+
+        public IServiceProvider ApplicationServices
+        {
+            get
+            {
+                return GetProperty<IServiceProvider>(ApplicationServicesKey);
+            }
+            set
+            {
+                SetProperty<IServiceProvider>(ApplicationServicesKey, value);
+            }
+        }
+
+        public IFeatureCollection ServerFeatures
+        {
+            get
+            {
+                return GetProperty<IFeatureCollection>(ServerFeaturesKey);
+            }
+        }
 
         public IDictionary<string, object> Properties { get; }
 
-        public IFeatureCollection ServerFeatures { get; private set; }
-
-        public Func<TContext, Task> Build()
+        private T GetProperty<T>(string key)
         {
-            throw new NotImplementedException();
+            object value;
+            return Properties.TryGetValue(key, out value) ? (T)value : default(T);
         }
 
-        public IApplicationBuilder<TContext> New()
+        private void SetProperty<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            Properties[key] = value;
         }
 
         public IApplicationBuilder<TContext> Use(Func<Func<TContext, Task>, Func<TContext, Task>> middleware)
         {
-            throw new NotImplementedException();
+            _components.Add(middleware);
+            return this;
+        }
+
+        public IApplicationBuilder<TContext> New()
+        {
+            return new ApplicationBuilder<TContext>(this);
+        }
+
+        public Func<TContext, Task> Build()
+        {
+            Func<TContext, Task> app = ctx => Task.FromResult(0);
+
+            foreach (var component in _components.Reverse())
+            {
+                app = component(app);
+            }
+
+            return app;
         }
     }
 }
